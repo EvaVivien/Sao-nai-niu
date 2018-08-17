cfbdfc061e8f2049c7824fe141c191b8eed58078
EvaViven <42457440+EvaViven@users.noreply.github.com>
2018-08-17 12:15:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 
+var fallInLoveMoment = moment('2016-02-26 19:00:00')
+
+var diff = fallInLoveMoment.fromNow()
+document.getElementById('conclusion').innerText = 'We start the relationship ' + diff + ','
+
+diff = moment().diff(fallInLoveMoment, 'day')
+document.getElementById('counting-d').innerText = 'which is ' + diff + ' days,'
+
+var sCountingDiv = document.getElementById('counting-s')
+function updateSecond() {
+	diff = moment().diff(fallInLoveMoment, 'second')
+	sCountingDiv.innerText = 'or ' + diff + ' seconds,'
+}
+function updateSecondSetTimeout() {
+	updateSecond()
+	setTimeout(updateSecondSetTimeout, 147)
+}
+
+updateSecondSetTimeout()
